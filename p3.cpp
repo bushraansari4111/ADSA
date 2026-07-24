ac42dc0af3be0d3551bb6e0eab81fd75bc0093b5
Bushra Ansari <168510938+bushraansari4111@users.noreply.github.com>
2026-07-24 09:42:17 +0530

    Hash of Char

---
+++ p3.cpp
@@ -0,0 +1,29 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int hasIndx(int n,int x){
+    return n%x;
+}
+int has(char ch,int x){
+    return ch%x;
+    
+}
+
+int main() {
+    int n,x;
+    char ch;
+    
+    cout<<"Enter the Table Size :"<<endl;
+    cin>>x;
+    if(x==0){
+        cout<<"Table size cannot be zero"<<endl;
+        return 0;
+    }
+    cout<<"Enter the key value :"<<endl;
+    cin>>ch;
+    cout<<"Has Index = "<<has(ch,x)<<endl;
+    
+
+    return 0;
+}
